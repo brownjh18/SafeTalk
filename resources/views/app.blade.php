5d81dcf0026773cbe0f0518bf50719a3c2b11213
brownjh18 <brownjh18@gmail.com>
2025-10-29 19:52:44 +0300

    Include CSS file in Vite directive to ensure proper asset loading

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,7 +42,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
-        @vite(['resources/js/app.tsx'], 'build')
+        @vite(['resources/css/app.css', 'resources/js/app.tsx'], 'build')
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
