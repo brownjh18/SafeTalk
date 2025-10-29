68be20c840283b328ac92cba2484fbd145d77fdd
brownjh18 <brownjh18@gmail.com>
2025-10-29 16:26:50 +0300

    Force HTTPS URLs in production and update Vite base URL configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: process.env.ASSET_URL || '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
